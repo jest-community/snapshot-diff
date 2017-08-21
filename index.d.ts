5c1d3195362fafa8fe8d345e7a8b405dc54964fb
Derek Sifford <dereksifford@gmail.com>
2017-08-21 11:36:58 -0400

    fix small issues in typescript types

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,13 @@ type DiffOptions = {
 };
 
 declare namespace jest {
-  interface Matchers {
-    toMatchDiffSnapshot(valueB: any, options?: DiffOptions): boolean
+  interface Matchers<R> {
+    toMatchDiffSnapshot(valueB: any, options?: DiffOptions): R
   }
+}
+
+declare module 'snapshot-diff' {
+    function diff(a: object, b: object): string;
+    namespace diff {}
+    export = diff;
 }
