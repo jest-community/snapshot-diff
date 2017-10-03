6ab5c5d7ad863936af953533ccbd5664c483cbb6
Orta <orta.therox+github@gmail.com>
2017-10-03 14:05:27 -0400

    Update index.d.ts (#17)

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,33 @@
 /// <reference types="jest"/>
 
 type DiffOptions = {
-  expand?: boolean,
-  colors?: boolean,
-  contextLines?: number,
-};
+  expand?: boolean
+  colors?: boolean
+  contextLines?: number
+}
 
 declare namespace jest {
   interface Matchers<R> {
+    /**
+     * Compare the difference between the actual in the `expect()`
+     * vs the object inside `valueB` with some extra options.
+     */
     toMatchDiffSnapshot(valueB: any, options?: DiffOptions): R
   }
 }
 
-declare module 'snapshot-diff' {
-  function diff(a: any, b: any, options?: DiffOptions): string;
-  namespace diff {}
-  export = diff;
+declare module "snapshot-diff" {
+  interface SnapshotDiff {
+    /**
+     * Compare the changes from a, to b
+     */
+    (a: any, b: any, options?: DiffOptions): string
+    /**
+     * Allows you to pull out toMatchDiffSnapshot and
+     * make it available via `expect.extend({ toMatchDiffSnapshot })`.
+     */
+    toMatchDiffSnapshot: (newObject: any) => void
+  }
+  const diff: SnapshotDiff
+  export = diff
 }
